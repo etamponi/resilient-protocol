ab8492553e357c6785e54ff8ae921d705bcf0f95
Emanuele Tamponi <emanuele.tamponi@diee.unica.it>
2014-03-04 13:59:40 +0000

    Remove old code

--- resilient/weighting_strategies.py
+++ resilient/weighting_strategies.py
@@ -1,12 +1,8 @@
 from abc import ABCMeta, abstractmethod
 
-from numpy.core.numeric import asarray
-from numpy.core.umath import sign
 import numpy as np
 import scipy.spatial.distance
 from sklearn.base import BaseEstimator
-from sklearn.metrics.metrics import accuracy_score
-from sklearn.utils.validation import array2d
 
 
 __author__ = 'Emanuele Tamponi <emanuele.tamponi@diee.unica.it>'
@@ -26,42 +22,6 @@ class WeightingStrategy(BaseEstimator):
     @abstractmethod
     def weight_classifiers(self, x):
         pass
-
-
-class LocalScoreWeightingStrategy(WeightingStrategy):
-
-    def __init__(self, k=15, scoring=accuracy_score):
-        self.k = k
-        self.scoring = scoring
-        # Training time attributes
-        self.classifiers_ = None
-
-    def prepare(self, inp, y):
-        self.classifiers_ = []
-
-    def add_estimator(self, est, train_set, test_set):
-        self.classifiers_.append((est, test_set))
-        return self
-
-    def weight_classifiers(self, x):
-        scores = np.zeros(len(self.classifiers_))
-        for i, (cls, ds) in enumerate(self.classifiers_):
-            inp, y = self._get_neighborhood(x, ds)
-            y_pred = cls.predict(inp)
-            try:
-                scores[i] = self.scoring(y, y_pred)
-            except ValueError:
-                pass
-        return scores
-
-    def _get_neighborhood(self, x, dataset):
-        sorting = []
-        for inst, targ in dataset:
-            sorting.append((scipy.spatial.distance.sqeuclidean(x, inst), inst, targ))
-        sorting.sort(cmp=lambda a, b: int(sign(a[0] - b[0])))
-        inp = [sample[1] for sample in sorting[:self.k]]
-        y = [sample[2] for sample in sorting[:self.k]]
-        return array2d(inp), asarray(y)
 
 
 class CentroidBasedWeightingStrategy(WeightingStrategy):
