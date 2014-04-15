ad570ee9fa71b18b02c1951f02ac4f8a67a2a1bb
Emanuele Tamponi <emanuele.tamponi@gmail.com>
2014-04-15 13:38:52 +0100

    Tagging version 0.2

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='resilient-protocol',
+    version='0.2',
+    packages=['arac', 'arac.tests', 'configs', 'resilient'],
+    url='',
+    license='',
+    author='Emanuele Tamponi',
+    author_email='emanuele.tamponi@gmail.com',
+    description='Resilient Ensemble implementation using scikit-learn'
+)
