6a2bb4d5463a384304a6ef19795d8fe57058f766
Emanuele Tamponi <emanuele.tamponi@gmail.com>
2014-04-15 13:55:11 +0100

    Change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='resilient-protocol',
-    version='0.2',
+    version='v0.2',
     packages=['arac', 'arac.tests', 'configs', 'resilient'],
     url='',
     license='',
